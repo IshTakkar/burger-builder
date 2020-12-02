aef25aed1ecc02e9a3720dfb8b45d952c23d501e
Ish Takkar <ishtakkar21@gmail.com>
2020-12-02 18:44:45 +0530

    deployed and added screenshots

---
+++ build/precache-manifest.f0c38af5c45a3fe9833fce1fabac58cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8f829f4912c41368f04b058190a6cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fff4a470f01cea04d59",
+    "url": "/static/css/main.f2e03495.chunk.css"
+  },
+  {
+    "revision": "d3b32d2a64144768eb31",
+    "url": "/static/js/2.06df8b9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.06df8b9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fff4a470f01cea04d59",
+    "url": "/static/js/main.24fd2fd8.chunk.js"
+  },
+  {
+    "revision": "54fa86d2aea86a318381",
+    "url": "/static/js/runtime-main.5c3b6bd7.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
